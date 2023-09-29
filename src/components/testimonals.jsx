b8b278d214e5720f97d20d2734ae9e9270668e6b
Krishnapriya MA <krishnapriyama185@gmail.com>
2023-09-29 18:30:10 +0530

    testimonal margin modified in mobile screen

--- src/components/testimonals.jsx
+++ src/components/testimonals.jsx
@@ -55,7 +55,7 @@ const Testimonials = () => {
   };
 
   return (
-    <div className="w-full flex mt-24 mb-10 flex-col justify-center bg-white">
+    <div className="w-full flex mt-10 flex-col justify-center bg-white">
       <div className="w-full">
         <div className="flex justify-center">
           <button className="bg-[#2F57EF] font-bold uppercase bg-opacity-[13%] text-[#2F57EF] px-5 py-2 rounded-3xl text-[16px]">
